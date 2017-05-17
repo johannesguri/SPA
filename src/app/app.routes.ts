58154991aefae02050ca94f5e605180e33a2075e
Johannes M <johannes.monsalve@ceiba.com.co>
2017-05-17 14:28:10 -0500

    componente Heroe y router desde heroes a heroe/0 o hero/1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,4 @@
+import { HeroeComponent } from './components/heroe/heroe.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
 import { AboutComponent } from './components/about/about.component';
 import { HomeComponent } from './components/home/home.component';
@@ -9,6 +10,7 @@ const APP_ROUTES: Routes = [
     { path: 'home', component: HomeComponent },
     { path: 'about', component: AboutComponent },
     { path: 'heroes', component: HeroesComponent },
+    { path: 'heroes/heroe/:id', component: HeroeComponent },
     { path: '**', pathMatch: 'full', redirectTo:'home' }
 ];
 
---
+++ src/app/components/heroe/heroe.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'app-heroe',
+    templateUrl: './heroe.component.html'
+})
+
+export class HeroeComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
