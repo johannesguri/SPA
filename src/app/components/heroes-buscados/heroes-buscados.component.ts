3e4ac0d4b2a9badc92ec051ad3e96174c39169a6
Johannes Jovins Monsalve Giraldo <johamogi@suramericana.com.co>
2017-06-21 12:47:33 -0500

    sube lo ultim

--- src/app/components/heroes-buscados/heroes-buscados.component.ts
+++ src/app/components/heroes-buscados/heroes-buscados.component.ts
@@ -10,17 +10,18 @@ import { Component, OnInit } from '@angular/core';
 export class HeroesBuscadosComponent implements OnInit {
 
   newArrayHeroesBuscados:Heroe[] = [];
-
+  termino:string;
+  msjNoHayResultados:boolean = false;
   constructor(private heroesService:HeroesService, 
               private activatedRoute:ActivatedRoute,
-              private router:Router) { 
-      this.activatedRoute.params.subscribe(params => {        
+              private router:Router) { }
+
+  ngOnInit() {
+     this.activatedRoute.params.subscribe(params => {   
+          this.termino = params['termino'];
           this.newArrayHeroesBuscados = this.heroesService.buscarHeroes(params['termino']);
         }
       );
-  }
-
-  ngOnInit() {
     
   }
 }
